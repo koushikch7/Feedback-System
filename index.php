eddd6ecf1efaa2a9de5e0bcbe56fc7f2e7a46fa4
KOUSHIK CH <koushik.ch7@gmail.com>
2019-04-07 22:17:35 +0530

    testing

--- index.php
+++ index.php
@@ -42,29 +42,29 @@ $(document).ready(function(){
      <input type="text" class="form-control" id="usnno" placeholder="Enter USN/Registered Number" name="usnno">
      <input type="hidden" name="role" id="role" value="student">
      <div class="form-group">
-     <label for="pwd">Password:</label>
+     <label for="pwd">Password :</label>
      <input type="password" class="form-control" id="pass" placeholder="Enter password" name="pass">
     </div>
 <input id="stu_log" class="btn btn-primary btn-block" type="submit" value="LOGIN"/>
 </form> 
 
 <form action="login.php" id="dept" method="POST">
-    <label>Department Id:</label>
+    <label>Department Id :</label>
      <input type="text" class="form-control" id="deptid" placeholder="Enter Department id" name="deptid">
      <input type="hidden" name="role" id="role" value="dept">
      <div class="form-group">
-     <label for="pwd">Password:</label>
+     <label for="pwd">Password :</label>
      <input type="password" class="form-control" id="pass1" placeholder="Enter password" name="pass1">
     </div>
 <input id="dept_log" class="btn btn-primary btn-block" type="submit" value="LOGIN"/>
 </form>
             
 <form action="login.php" id="admin" method="POST">
-     <label>Username:</label>
+     <label>Username :</label>
      <input type="text" class="form-control" id="adminid" placeholder="Enter Username" name="adminid">
      <input type="hidden" name="role" id="role" value="admin">
      <div class="form-group">
-     <label for="pwd">Password:</label>
+     <label for="pwd">Password :</label>
      <input type="password" class="form-control" id="pass2" placeholder="Enter password" name="pass2">
     </div>
    <input name="admin_log" class="btn btn-primary btn-block" type="submit" value="Login"/>
